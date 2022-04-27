31973209d75ab7faeefcbba14b087b36933e4d9b
Advorix <whatever1692@gmail.com>
2022-04-27 09:04:53 -0500

    Add main game functionality

--- script.js
+++ script.js
@@ -8,3 +8,27 @@ function computerPlay() {
     }
     return choice;
 }
+function capitalize(word) {
+    return word[0].toUpperCase() + word.substring(1);
+}
+function playRound(computer, human) {
+    human = capitalize(human);
+    if (computer === human) {
+        return "It's a tie. " + computer + " and " + human;
+    }
+    else if ((computer === "Rock" && human === "Scissors") || 
+             (computer === "Paper" && human === "Rock") || 
+             (computer === "Scissors" && human === "Paper")) {
+                 return "Computer wins. " + computer + " beats " + human + ".";
+             }
+    else {
+        return "You win. " + human + " beats " + computer + ".";
+    }
+
+}
+
+computerChoice = computerPlay();
+playerChoice = (prompt("Rock, paper, or scissors?")).toLowerCase();
+console.log(playRound(computerChoice, playerChoice));
+
+
