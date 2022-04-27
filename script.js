c0d69cf1cdacfabb009f06eed1fea34bf81f56e4
Advorix <whatever1692@gmail.com>
2022-04-27 07:42:42 -0500

    Add function for computer choice

---
+++ script.js
@@ -0,0 +1,10 @@
+function computerPlay() {
+    let choice = Math.floor(Math.random() * 3);
+    
+    switch(choice) {
+        case 0: choice = "Rock"; break;
+        case 1: choice = "Paper"; break;
+        case 2: choice = "Scissors"; break;
+    }
+    return choice;
+}
